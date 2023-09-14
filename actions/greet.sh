5129f3f3e457cc9cbf608cf00dea1ea2504fae15
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-14 17:28:16 +0530

    10: testing linux capacity SOP.

--- actions/greet.sh
+++ actions/greet.sh
@@ -4,10 +4,10 @@ OUTPUT_FILE="/opt/stackstorm/output.txt"
 
 {
     echo "date: $(date)" >> "$OUTPUT_FILE"
-    echo "uname: $(uname -a | awk '{print $2}')" >> "$OUTPUT_FILE"
-    echo "uptime: $(uptime | awk -F "up |, | users" '{print $2}')" >> "$OUTPUT_FILE"
+    echo "uname: $(sudo uname -a | awk '{print $2}')" >> "$OUTPUT_FILE"
+    echo "uptime: $(sudo uptime | awk -F "up |, | users" '{print $2}')" >> "$OUTPUT_FILE"
 
-    top_output=$(top -b -n 1 -o %CPU)
+    top_output=$(sudo top -b -n 1 -o %CPU)
     echo "Load Average: $(echo "$top_output" | grep -E 'load average:' | awk '{print $10, $11, $12}')" >> "$OUTPUT_FILE"
     echo "Process ID: $(echo "$top_output" | grep -E '^\s*[0-9]+' | head -n 1 | awk '{print $1}')" >> "$OUTPUT_FILE"
     echo "User/Owner: $(echo "$top_output" | grep -E '^\s*[0-9]+' | head -n 1 | awk '{print $1}')" >> "$OUTPUT_FILE"
