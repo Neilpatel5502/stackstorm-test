0be88c137b161c68ae4ac249b2c9165da861b160
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-14 20:15:53 +0530

    25: testing linux capacity SOP.

--- actions/clean_output.py
+++ actions/clean_output.py
@@ -13,8 +13,8 @@ class MyPythonAction(AWSBaseAction):
         uptime_out = output[2]
         load_average = output[3]
         process_id, cpu_user, cpu_percent, command = output[4].split(" ")
-        sar_cpu_free = float(output[5])
-        sar_cpu_utilization = 100 - sar_cpu_free
+        sar_cpu_free = output[5]
+        # sar_cpu_utilization = 100 - sar_cpu_free
         sar_mem_utilization = output[6]
         sar_swap_utilization = output[7]
         sar_sys_processes, sar_sys_threads, sys_load_1, sys_load_2, sys_load_3 = output[8].split(" ")
@@ -31,7 +31,7 @@ class MyPythonAction(AWSBaseAction):
             "CPU Utilization": cpu_percent,
             "Command/Application": command,
             "SAR Free/Idle": f"{sar_cpu_free} (Average)",
-            "SAR CPU Utilization": f"{sar_cpu_utilization} (Average)",
+            "SAR CPU Utilization": f"test (Average)",
             "SAR MEM Utilization": f"{sar_mem_utilization} (Average)",
             "SAR SWAP Utilization": f"{sar_swap_utilization} (Average)",
             "Processes in Running/Queue": sar_sys_processes,
