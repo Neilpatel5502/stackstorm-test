2b621392c1f6d8d2a46b8ff5ff6605d091162936
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-14 18:41:59 +0530

    16: testing linux capacity SOP.

--- actions/clean_output.py
+++ actions/clean_output.py
@@ -2,6 +2,8 @@ from st2common.runners.base_action import Action
 
 class MypythonAction(Action):
     def run(self, args):
-        print(args)
+        
+        with open("output.txt", "w") as file:
+            file.write(f"{args}")
 
         return args
