7b7e19d49340c9965b8a3f4181aa19b9c438755a
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-14 20:09:07 +0530

    21: testing linux capacity SOP.

--- actions/clean_output.py
+++ actions/clean_output.py
@@ -1,3 +1,4 @@
+import math
 from lib.actions import AWSBaseAction
 
 __all__ = [
@@ -12,6 +13,13 @@ class MyPythonAction(AWSBaseAction):
         uptime_out = output[2]
         load_average = output[3]
         process_id, cpu_user, cpu_percent, command = output[4].split(" ")
+        sar_cpu_free = int(output[5])
+        sar_cpu_utilization = 100 - sar_cpu_free
+        sar_mem_utilization = output[6]
+        sar_swap_utilization = output[7]
+        sar_sys_processes, sar_sys_threads, sys_load_1, sys_load_2, sys_load_3 = output[8].split(" ")
+        sar_sysload_avg_min = math.ceil(min(float(sys_load_1), float(sys_load_2), float(sys_load_3)))
+        sar_sysload_avg_max = math.floor(max(float(sys_load_1), float(sys_load_2), float(sys_load_3)))
 
         output = {
             "date": date_out,
@@ -22,5 +30,12 @@ class MyPythonAction(AWSBaseAction):
             "User/Owner": cpu_user,
             "CPU Utilization": cpu_percent,
             "Command/Application": command,
+            "SAR Free/Idle%": f"{sar_cpu_free} (Average)",
+            "SAR CPU Utilization": f"{sar_cpu_utilization} (Average)",
+            "SAR MEM Utilization": f"{sar_mem_utilization} (Average)",
+            "SAR SWAP Utilization": f"{sar_swap_utilization} (Average)",
+            "Processes in Running/Queue": sar_sys_processes,
+            "threads in Running/Queue": sar_sys_threads,
+            "System Load Average is in between": f"{sar_sysload_avg_min}~{sar_sysload_avg_max}",
         }
         return output
