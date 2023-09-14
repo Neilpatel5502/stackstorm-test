2685a68108484f193bbe40c7bb12197c94093808
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-14 17:01:42 +0530

    7: testing linux capacity SOP.

--- actions/greet.sh
+++ actions/greet.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 date
-uname -a
-uptime
+uname -a | awk '{print $2}'
+uptime | awk -F "up |, | users" '{print $2}'
 
-free -h
-free -k
-
-top -o %MEM
+top -b -n 1 -o %CPU | grep -E "load average:" | awk '{print $8, $9, $10, $11, $12}'
+top -b -n 1 -o %CPU | grep -E '^\s*[0-9]+' | head -n 1 | awk '{print $1, $2, $9, $12}'
