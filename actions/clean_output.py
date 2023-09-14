53c2d1e17323146c2262149be970d9d525e27329
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-14 19:07:45 +0530

    19: testing linux capacity SOP.

--- actions/clean_output.py
+++ actions/clean_output.py
@@ -7,4 +7,20 @@ __all__ = [
 class MyPythonAction(AWSBaseAction):
     def run(self, args):
         output = args.split("\n")
+        date_out = output[0]
+        uname_out = output[1]
+        uptime_out = output[2]
+        load_average = output[3]
+        process_id, cpu_user, cpu_percent, command = output[4].split(" ")
+
+        output = {
+            "date": date_out,
+            "uname": uname_out,
+            "uptime": uptime_out,
+            "Load Average": load_average,
+            "Process ID": process_id,
+            "User/Owner": cpu_user,
+            "CPU Utilization": cpu_percent,
+            "Command/Application": command,
+        }
         return output
