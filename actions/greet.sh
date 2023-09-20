8de03800672a7f57bacf55bd8c6ceb2f01fb9585
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-20 10:01:02 +0530

    Fixed paranthesis issue

--- actions/greet.sh
+++ actions/greet.sh
@@ -4,11 +4,9 @@
 output=$(date)
 output="$output\n$(uname -a | awk '{print $2}')"
 output="$output\n$(uptime | awk -F 'up |, | users' '{print $2}')"
-output="$output\n$(top -b -n 1 -o %CPU | grep -oP 'load average: \K.*$'"
+output="$output\n$(top -b -n 1 -o %CPU | grep -oP 'load average: \K.*$')"
 output="$output\n$(top -b -n 1 -o %CPU | grep -E '^\s*[0-9]+' | head -n 1 | awk '{print $1, $2, $9, $12}')"
-
 output="$output\n$(sar -u 5 5 | grep -E "Average:" | awk '{print $8}')"
-
 output="$output\n$(sar -r 5 5 | grep -E "Average:" | awk '{print $5}')"
 
 output="$output\n$(sar -S 5 5 | grep -E "Average:" | awk '{print $4}')"
