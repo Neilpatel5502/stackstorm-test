b891d76777c4c9d21bade647ff0ed2ab9d790574
NeelPatel <nilu.neelpatel@gmail.com>
2023-09-26 08:01:24 +0530

    44: testing linux capacity SOP.

--- actions/ssh_with_password.py
+++ actions/ssh_with_password.py
@@ -2,6 +2,12 @@ import paramiko
 from st2common.runners.base_action import Action
 
 class SSHWithPasswordAction(Action):
+    def execute_command(self, ssh, command):
+        _, stdout, _ = ssh.exec_command(command)
+        output = stdout.read().decode('utf-8')
+        return output
+
+
     def run(self, ec2_instance_ip, username, password):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -9,10 +15,24 @@ class SSHWithPasswordAction(Action):
         try:
             ssh.connect(ec2_instance_ip, username=username, password=password)
             
-            command = "ls -l"
-            stdin, stdout, stderr = ssh.exec_command(command)
+            date = self.execute_command(ssh, "date")
+            uname = self.execute_command(ssh, "uname -a | awk '{print $2}'")
+            uptime = self.execute_command(ssh, "uptime | awk -F 'up |, | users' '{print $2}'")
+            load_average = self.execute_command(ssh, "top -b -n 1 -o %CPU | grep -oP 'load average: \K.*$'")
+            cpu_process_data = self.execute_command(ssh, "top -b -n 1 -o %CPU | grep -E '^\s*[0-9]+' | head -n 1 | awk '{print $1, $2, $9, $12}'")
+            process_id, cpu_user, cpu_percent, command = cpu_process_data.split(" ")
 
-            output = stdout.read().decode('utf-8')
+            output = {
+                "date": date,
+                "uname": uname,
+                "uptime": uptime,
+                "Load Average": load_average,
+                "Process ID": process_id,
+                "User/Owner": cpu_user,
+                "CPU Utilization": cpu_percent,
+                "Command/Application": command,
+            }
+
             return (True, output)
         except Exception as e:
             return (False, str(e))
